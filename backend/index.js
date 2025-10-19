f6a1a5c2e3bc3d00b6f56dacd47203c00297d4a3
robertstb <robert.stenbacka@gmail.com>
2025-10-19 11:37:52 +0300

    Updated the port for the backend

--- backend/index.js
+++ backend/index.js
@@ -76,7 +76,8 @@ app.post('/api/persons', morgan(':method :url :status :res[content-length] - :re
   }
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
