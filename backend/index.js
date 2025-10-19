5aaf0245aaa70a1798df52fe7209d49c6c43eb49
robertstb <robert.stenbacka@gmail.com>
2025-10-19 12:06:26 +0300

    Adding instruction to use 'dist' file for static page

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,9 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 morgan.token('body', (req, res) => {return JSON.stringify(req.body)})
+
 
 let people = [
     { 
